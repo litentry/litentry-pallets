2fc7724a69c25454b0ac8d3db3c896208c91b7ae
Satoshi-Kusumoto <29630164+Satoshi-Kusumoto@users.noreply.github.com>
2021-05-15 14:02:37 +0800

    initalize nft pallet

---
+++ pallets/nft/src/lib.rs
@@ -0,0 +1,259 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use enumflags2::BitFlags;
+use frame_support::{
+	pallet_prelude::*,
+	transactional,
+};
+use frame_system::pallet_prelude::*;
+use orml_traits::NFT;
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_runtime::{
+	traits::StaticLookup,
+	DispatchResult, RuntimeDebug,
+};
+use sp_std::vec::Vec;
+
+pub mod weights;
+
+pub use pallet::*;
+pub use weights::WeightInfo;
+
+pub type CID = Vec<u8>;
+
+#[repr(u8)]
+#[derive(Encode, Decode, Clone, Copy, BitFlags, RuntimeDebug, PartialEq, Eq)]
+pub enum ClassProperty {
+	/// Token can be transferred
+	Transferable = 0b00000001,
+	/// Token can be burned
+	Burnable = 0b00000010,
+}
+
+#[derive(Clone, Copy, PartialEq, Default, RuntimeDebug)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct Properties(pub BitFlags<ClassProperty>);
+
+impl Eq for Properties {}
+impl Encode for Properties {
+	fn using_encoded<R, F: FnOnce(&[u8]) -> R>(&self, f: F) -> R {
+		self.0.bits().using_encoded(f)
+	}
+}
+impl Decode for Properties {
+	fn decode<I: codec::Input>(input: &mut I) -> sp_std::result::Result<Self, codec::Error> {
+		let field = u8::decode(input)?;
+		Ok(Self(
+			<BitFlags<ClassProperty>>::from_bits(field as u8).map_err(|_| "invalid value")?,
+		))
+	}
+}
+
+#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct ClassData {
+	/// Property of token
+	pub properties: Properties,
+    /// Maximum number of NFT
+    pub max_amount: u8,
+}
+
+#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct TokenData {}
+
+pub type TokenIdOf<T> = <T as orml_nft::Config>::TokenId;
+pub type ClassIdOf<T> = <T as orml_nft::Config>::ClassId;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+
+	#[pallet::config]
+	pub trait Config:
+		frame_system::Config
+		+ orml_nft::Config<ClassData = ClassData, TokenData = TokenData>
+	{
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// Weight information for the extrinsics in this module.
+		type WeightInfo: WeightInfo;
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// ClassId not found
+		ClassIdNotFound,
+		/// TokenId not found
+		TokenIdNotFound,
+		/// The operator is not the owner of the token and has no permission
+		NoPermission,
+		/// Quantity is invalid. need >= 1
+		InvalidQuantity,
+		/// Property of class don't support transfer
+		NonTransferable,
+		/// Property of class don't support burn
+		NonBurnable,
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Created NFT class. \[owner, class_id\]
+		CreatedClass(T::AccountId, ClassIdOf<T>),
+		/// Minted NFT token. \[from, to, class_id, quantity\]
+		MintedToken(T::AccountId, T::AccountId, ClassIdOf<T>, u32),
+		/// Transferred NFT token. \[from, to, class_id, token_id\]
+		TransferredToken(T::AccountId, T::AccountId, ClassIdOf<T>, TokenIdOf<T>),
+		/// Burned NFT token. \[owner, class_id, token_id\]
+		BurnedToken(T::AccountId, ClassIdOf<T>, TokenIdOf<T>),
+		/// Burned NFT token with remark. \[owner, class_id, token_id, remark_hash\]
+		BurnedTokenWithRemark(T::AccountId, ClassIdOf<T>, TokenIdOf<T>, T::Hash),
+		/// Destroyed NFT class. \[owner, class_id\]
+		DestroyedClass(T::AccountId, ClassIdOf<T>),
+	}
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Create NFT class, tokens belong to the class.
+		///
+		/// - `metadata`: external metadata
+		/// - `properties`: class property, include `Transferable` `Burnable`
+		#[pallet::weight(<T as Config>::WeightInfo::create_class())]
+		#[transactional]
+		pub fn create_class(origin: OriginFor<T>, metadata: CID, properties: Properties, max_amount: u8) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?; // TODO who can?
+			let next_id = orml_nft::Pallet::<T>::next_class_id();
+
+			let data = ClassData {
+				properties,
+                max_amount,
+			};
+			orml_nft::Pallet::<T>::create_class(&who, metadata, data)?;
+
+			Self::deposit_event(Event::CreatedClass(who, next_id));
+			Ok(().into())
+		}
+
+		/// Mint NFT token
+		///
+		/// - `to`: the token owner's account
+		/// - `class_id`: token belong to the class id
+		/// - `metadata`: external metadata
+		/// - `quantity`: token quantity
+		#[pallet::weight(<T as Config>::WeightInfo::mint(*quantity))]
+		#[transactional]
+		pub fn mint(
+			origin: OriginFor<T>,
+			to: <T::Lookup as StaticLookup>::Source, // TODO why
+			class_id: ClassIdOf<T>,
+			metadata: CID,
+			quantity: u32,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			let to = T::Lookup::lookup(to)?;
+			ensure!(quantity >= 1, Error::<T>::InvalidQuantity);
+			let class_info = orml_nft::Pallet::<T>::classes(class_id).ok_or(Error::<T>::ClassIdNotFound)?;
+			ensure!(who == class_info.owner, Error::<T>::NoPermission);
+
+			let data = TokenData {};
+			for _ in 0..quantity {
+				orml_nft::Pallet::<T>::mint(&to, class_id, metadata.clone(), data.clone())?;
+			}
+
+			Self::deposit_event(Event::MintedToken(who, to, class_id, quantity));
+			Ok(().into())
+		}
+
+		/// Transfer NFT token to another account
+		///
+		/// - `to`: the token owner's account
+		/// - `token`: (class_id, token_id)
+		#[pallet::weight(<T as Config>::WeightInfo::transfer())]
+		#[transactional]
+		pub fn transfer(
+			origin: OriginFor<T>,
+			to: <T::Lookup as StaticLookup>::Source,
+			token: (ClassIdOf<T>, TokenIdOf<T>),
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			let to = T::Lookup::lookup(to)?;
+			Self::do_transfer(&who, &to, token)?;
+			Ok(().into())
+		}
+
+		/// Burn NFT token
+		///
+		/// - `token`: (class_id, token_id)
+		#[pallet::weight(<T as Config>::WeightInfo::burn())]
+		#[transactional]
+		pub fn burn(origin: OriginFor<T>, token: (ClassIdOf<T>, TokenIdOf<T>)) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			Self::do_burn(&who, token)?;
+			Self::deposit_event(Event::BurnedToken(who, token.0, token.1));
+			Ok(().into())
+		}
+
+	}
+}
+
+impl<T: Config> Pallet<T> {
+	/// Ensured atomic.
+	#[transactional]
+	fn do_transfer(from: &T::AccountId, to: &T::AccountId, token: (ClassIdOf<T>, TokenIdOf<T>)) -> DispatchResult {
+		let class_info = orml_nft::Pallet::<T>::classes(token.0).ok_or(Error::<T>::ClassIdNotFound)?;
+		let data = class_info.data;
+		ensure!(
+			data.properties.0.contains(ClassProperty::Transferable),
+			Error::<T>::NonTransferable
+		);
+
+		orml_nft::Pallet::<T>::transfer(from, to, token)?;
+
+		Self::deposit_event(Event::TransferredToken(from.clone(), to.clone(), token.0, token.1));
+		Ok(())
+	}
+
+	/// Ensured atomic.
+	#[transactional]
+	fn do_burn(who: &T::AccountId, token: (ClassIdOf<T>, TokenIdOf<T>)) -> DispatchResult {
+		let class_info = orml_nft::Pallet::<T>::classes(token.0).ok_or(Error::<T>::ClassIdNotFound)?;
+		let data = class_info.data;
+		ensure!(
+			data.properties.0.contains(ClassProperty::Burnable),
+			Error::<T>::NonBurnable
+		);
+
+		let token_info = orml_nft::Pallet::<T>::tokens(token.0, token.1).ok_or(Error::<T>::TokenIdNotFound)?;
+		ensure!(*who == token_info.owner, Error::<T>::NoPermission);
+
+		orml_nft::Pallet::<T>::burn(&who, token)?;
+
+		Ok(())
+	}
+}
+
+impl<T: Config> NFT<T::AccountId> for Pallet<T> {
+	type ClassId = ClassIdOf<T>;
+	type TokenId = TokenIdOf<T>;
+	type Balance = u128;
+
+	fn balance(who: &T::AccountId) -> Self::Balance {
+		orml_nft::TokensByOwner::<T>::iter_prefix(who).count() as u128
+	}
+
+	fn owner(token: (Self::ClassId, Self::TokenId)) -> Option<T::AccountId> {
+		orml_nft::Pallet::<T>::tokens(token.0, token.1).map(|t| t.owner)
+	}
+
+	fn transfer(from: &T::AccountId, to: &T::AccountId, token: (Self::ClassId, Self::TokenId)) -> DispatchResult {
+		Self::do_transfer(from, to, token)
+	}
+}
