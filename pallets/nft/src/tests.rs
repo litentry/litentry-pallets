1f14e778bc2cb6b15aea7a0ef84af3b2ee448b40
Han Zhao <windzhaohan@gmail.com>
2021-06-21 17:34:21 +0200

    Add unit test for claim

--- pallets/nft/src/tests.rs
+++ pallets/nft/src/tests.rs
@@ -31,29 +31,36 @@ fn test_issue_and_mint_eth() {
 fn test_issue_and_claim_eth() {
 	new_test_ext().execute_with(|| {
 
-        // issue a claim class
-        assert_ok!(Nft::create_class(
+    // root is 0x784756769ecc8bdd4b2ac239c57c1fd56a20e272c88b37131ef84e1541d35c3c
+    let merkle_root = vec![0x78,0x47,0x56,0x76,0x9e,0xcc,0x8b,0xdd,0x4b,0x2a,0xc2,0x39,0xc5,0x7c,0x1f,0xd5,0x6a,0x20,0xe2,0x72,0xc8,0x8b,0x37,0x13,0x1e,0xf8,0x4e,0x15,0x41,0xd3,0x5c,0x3c];
+
+    // proof of alice is 0xf4ee0d453c09d8b28507010935ffbc9ccc41044a4e7237405dbc1313f9ad140b
+    let alice_proof = vec![[0xf4u8,0xeeu8,0x0du8,0x45u8,0x3cu8,0x09u8,0xd8u8,0xb2u8,0x85u8,0x07u8,0x01u8,0x09u8,0x35u8,0xffu8,0xbcu8,0x9cu8,0xccu8,0x41u8,0x04u8,0x4au8,0x4eu8,0x72u8,0x37u8,0x40u8,0x5du8,0xbcu8,0x13u8,0x13u8,0xf9u8,0xadu8,0x14u8,0x0bu8]];
+
+    // issue a claim class
+    assert_ok!(Nft::create_class(
 			Origin::signed(1),
-			CID::default(), 
+			merkle_root.clone(), 
 			Properties::default(), 
 			None,
 			None,
-			ClassType::Claim(CID::default()),
+			ClassType::Claim(merkle_root),
 		));
 
-        // assert_eq!(
+    // assert_eq!(
 		// 	events(),
 		// 	[
 		// 		Event::nft(crate::Event::CreatedClass(account.clone(), 0)),
 		// 	]
 		// );
 
-        // claim with proof
-        assert_ok!(Nft::claim(
+    // claim with proof
+    assert_ok!(Nft::claim(
 			Origin::signed(1),
-            0,
-            u32::default(),
-        ));
+      0,
+      0,
+      alice_proof,
+      ));
 
     })
 }
