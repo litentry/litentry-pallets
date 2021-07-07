aa507d27c7b5994c5193a7744804f51492f6fb32
h4n0 <4738254+h4n0@users.noreply.github.com>
2021-07-07 13:14:45 +0000

    Deploying to gh-pages from @ litentry/litentry-pallets@fb3b84c3935238f859c93d5dfe8fb902b39f9467 🚀

---
+++ weights/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["SubstrateWeight","Weights for pallet_account_linker using the Substrate node and recommended hardware."]],"trait":[["WeightInfo","Weight functions needed for pallet_account_linker."]]});
