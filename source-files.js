0d05abeca79919c080ddfe26022de1e9ca04e237
Satoshi-Kusumoto <29630164+Satoshi-Kusumoto@users.noreply.github.com>
2021-07-27 02:04:16 +0000

    Deploying to gh-pages from @ litentry/litentry-pallets@cef8ee3f953a15d14dbcf86e644590e71a80370a 🚀

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["pallet_account_linker"] = {"name":"","dirs":[{"name":"btc","files":["base58.rs","legacy.rs","mod.rs","witness.rs"]}],"files":["lib.rs","util_eth.rs","weights.rs"]};
-sourcesIndex["pallet_nft"] = {"name":"","files":["lib.rs","merkle_proof.rs","weights.rs"]};
-sourcesIndex["pallet_offchain_worker"] = {"name":"","files":["lib.rs","urls.rs","utils.rs","weights.rs"]};
+sourcesIndex["pallet_account_linker"] = {"name":"","dirs":[{"name":"btc","files":["base58.rs","legacy.rs","mod.rs","witness.rs"]}],"files":["benchmarking.rs","lib.rs","util_eth.rs","weights.rs"]};
+sourcesIndex["pallet_nft"] = {"name":"","files":["benchmarking.rs","lib.rs","merkle_proof.rs","weights.rs"]};
+sourcesIndex["pallet_offchain_worker"] = {"name":"","files":["benchmarking.rs","lib.rs","urls.rs","utils.rs","weights.rs"]};
 createSourceSidebar();
