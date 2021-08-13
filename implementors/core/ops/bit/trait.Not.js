8683d3288307ebc4ecf41dd0e0873f8e5123be5e
suinuj <74189262+suinuj@users.noreply.github.com>
2021-08-13 03:14:14 +0000

    Deploying to gh-pages from @ litentry/litentry-pallets@3a4be2f2070b46baa44e4c679ad5ca14c3dc3ade 🚀

--- implementors/core/ops/bit/trait.Not.js
+++ implementors/core/ops/bit/trait.Not.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pallet_nft"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"pallet_nft/enum.ClassProperty.html\" title=\"enum pallet_nft::ClassProperty\">ClassProperty</a>","synthetic":false,"types":["pallet_nft::ClassProperty"]}];
+implementors["pallet_nft"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"pallet_nft/enum.ClassProperty.html\" title=\"enum pallet_nft::ClassProperty\">ClassProperty</a>","synthetic":false,"types":["pallet_nft::ClassProperty"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
